16cc1e0702b22ead8ea6d1ba7450a97b09d7c3a1
chzh89 <chzh89@gmail.com>
2022-05-19 15:04:08 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="jgw.hbslyjt.tk";
+        url.hostname="168.138.47.176";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
