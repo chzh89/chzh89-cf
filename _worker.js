9b71ef23e0ecc19e5201b2b25476080c3d9113be
chzh89 <chzh89@gmail.com>
2022-05-24 15:51:41 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,7 +2,7 @@ export default {
     async fetch(request, env) {
       let url = new URL(request.url);
       if (url.pathname.startsWith('/')) {
-        url.hostname="168.138.47.176";
+        url.hostname="chzh89-4.herokuapp.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
       }
